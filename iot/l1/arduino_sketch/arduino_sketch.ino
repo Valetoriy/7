95023af4e56ffbf5ec3b03f8ad738104f087a967
Valetoriy <shadolproff420@mail.ru>
2024-09-17 01:12:48 +0300

    [iot] l1

---
+++ iot/l1/arduino_sketch/arduino_sketch.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    for (int i = 0; i < 255; i++) {
+        analogWrite(LED_BUILTIN, i);
+        delay(5);
+    }
+    for (int i = 255; i >= 0; i--) {
+        analogWrite(LED_BUILTIN, i);
+        delay(5);
+    }
+}
