fc15c352658e26407619f2c5af191b3327525df1
Valetoriy <shadolproff420@mail.ru>
2024-09-24 00:43:42 +0300

    [taifa] Код l2

--- taifa/l2/src/main.rs
+++ taifa/l2/src/main.rs
@@ -1,3 +1,62 @@
+use std::env::args;
+
+#[derive(Debug)]
+enum State {
+    Zero,
+    One,
+    Two,
+    Three,
+}
+
+/*
+ * /--\
+ * V  |(0, 1)
+ * 2 -/
+ * ^           /--\
+ * |(0)        V  |(0, 1)
+ * 1 --------> 3 -/
+ * ^  (1)      ^
+ * |(0)        |
+ * 0 ----------/
+ * ^  (1)
+ * |(start)
+*/
+fn check_input(input: &str) -> (State, bool) {
+    let mut state = State::Zero;
+    for c in input.chars() {
+        use State::*;
+        match c {
+            '0' => match state {
+                Zero => state = One,
+                One => state = Two,
+                Two => state = Two,
+                Three => state = Three,
+            },
+            '1' => match state {
+                Zero => state = Three,
+                One => state = Three,
+                Two => state = Two,
+                Three => state = Three,
+            },
+            _ => (),
+        }
+    }
+
+    if let State::Two = state {
+        (state, true)
+    } else {
+        (state, false)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = args().nth(1).expect("Не введено проверямое слово");
+    let (state, success) = check_input(&input);
+
+    println!("ЛР№2, 2 вариант. КА завершил работу в состоянии \"{state:?}\"");
+    if success {
+        println!("Слово соответствует языку");
+    } else {
+        println!("Слово не соответствует языку");
+    }
 }
