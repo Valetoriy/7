04a2f107d59ffb8e08f186e21bad62c4a2778770
Valetoriy <shadolproff420@mail.ru>
2024-09-25 20:43:36 +0300

    [taifa] l2

--- taifa/l2/src/main.rs
+++ taifa/l2/src/main.rs
@@ -1,48 +1,28 @@
 use std::env::args;
 
-#[derive(Debug)]
-enum State {
-    Zero,
-    One,
-    Two,
-    Three,
-}
-
-/*
- * /--\
- * V  |(0, 1)
- * 2 -/
- * ^           /--\
- * |(0)        V  |(0, 1)
- * 1 --------> 3 -/
- * ^  (1)      ^
- * |(0)        |
- * 0 ----------/
- * ^  (1)
- * |(start)
-*/
-fn check_input(input: &str) -> (State, bool) {
-    let mut state = State::Zero;
+fn check_input(input: &str) -> (char, bool) {
+    let mut state = '0';
     for c in input.chars() {
-        use State::*;
         match c {
             '0' => match state {
-                Zero => state = One,
-                One => state = Two,
-                Two => state = Two,
-                Three => state = Three,
+                '0' => state = '1',
+                '1' => state = '2',
+                '2' => state = '2',
+                '3' => state = '3',
+                _ => (),
             },
             '1' => match state {
-                Zero => state = Three,
-                One => state = Three,
-                Two => state = Two,
-                Three => state = Three,
+                '0' => state = '3',
+                '1' => state = '3',
+                '2' => state = '2',
+                '3' => state = '3',
+                _ => (),
             },
             _ => (),
         }
     }
 
-    if let State::Two = state {
+    if state == '2' {
         (state, true)
     } else {
         (state, false)
@@ -53,7 +33,7 @@ fn main() {
     let input = args().nth(1).expect("Не введено проверямое слово");
     let (state, success) = check_input(&input);
 
-    println!("ЛР№2, 2 вариант. КА завершил работу в состоянии \"{state:?}\"");
+    println!("ЛР№2, 2 вариант. КА завершил работу в состоянии \"{state}\"");
     if success {
         println!("Слово соответствует языку");
     } else {
